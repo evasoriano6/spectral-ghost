9b49f96f6d872e394dea281d92a812c7288305fe
Spectral Ghost User <lauraleomilan@gmail.com>
2026-02-18 12:33:32 +0100

    Fix JNI: Update signatures for new package structure

--- native/NativeBridge.cpp
+++ native/NativeBridge.cpp
@@ -5,7 +5,7 @@
 #include "ThermalFilter.cpp"
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_spectral_ghost_NativeHypervisor_stringFromJNI(
+Java_com_spectral_ghost_data_native_NativeHypervisor_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "SPECTRAL-01 NATIVE CORE ACTIVE";
@@ -14,23 +14,23 @@ Java_com_spectral_ghost_NativeHypervisor_stringFromJNI(
 
 // MARK: - AUDIO ENGINE BINDINGS
 extern "C" JNIEXPORT void JNICALL
-Java_com_spectral_ghost_NativeHypervisor_startAudioEngine(JNIEnv* env, jobject thiz) {
+Java_com_spectral_ghost_data_native_NativeHypervisor_startAudioEngine(JNIEnv* env, jobject thiz) {
     if (engine) engine->start();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_spectral_ghost_NativeHypervisor_stopAudioEngine(JNIEnv* env, jobject thiz) {
+Java_com_spectral_ghost_data_native_NativeHypervisor_stopAudioEngine(JNIEnv* env, jobject thiz) {
     if (engine) engine->stop();
 }
 
 // MARK: - VULKAN THERMAL FILTER BINDINGS
 extern "C" JNIEXPORT void JNICALL
-Java_com_spectral_ghost_NativeHypervisor_initVulkanFilter(JNIEnv* env, jobject thiz) {
+Java_com_spectral_ghost_data_native_NativeHypervisor_initVulkanFilter(JNIEnv* env, jobject thiz) {
     if (filter) filter->initVulkan();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_spectral_ghost_NativeHypervisor_processThermalFrame(JNIEnv* env, jobject thiz, jbyteArray data, jint w, jint h) {
+Java_com_spectral_ghost_data_native_NativeHypervisor_processThermalFrame(JNIEnv* env, jobject thiz, jbyteArray data, jint w, jint h) {
     // Convertir jbyteArray a uint8_t* para C++
     jboolean isCopy;
     jbyte* buffer = env->GetByteArrayElements(data, &isCopy);
